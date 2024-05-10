d281f88d0810225c0e33fc3acb4c599f012af101
rviry <rom88.viry@gmail.com>
2024-05-10 16:37:43 +0200

    (exceptions) add exception & refactor struct with http status

--- printer/exception.py
+++ printer/exception.py
@@ -5,20 +5,36 @@ class BrcdPrinterException(Exception):
 
 class QtyIncorrect(BrcdPrinterException):
     message = """Qantité incorrect. la quantité doit être entre 1 et {mx_qty}"""
+    status = 400
     def __init__(self, mx_qty: int) -> None:
         super().__init__(self.message.format(mx_qty=str(mx_qty)))
         
 class UnknownBarcodeFormat(BrcdPrinterException):
     message = """Le code-barres ne correspond à aucun format connu"""
+    status = 400
     def __init__(self) -> None:
         super().__init__(self.message)
         
 class NotAcceptedBarcodeFormat(BrcdPrinterException):
     message = """Les codes-barres de type "{btype}" ne sont pas acceptés."""
+    status = 400
     def __init__(self, btype: str) -> None:
         super().__init__(self.message.format(btype=btype))
         
 class ProductNotFound(BrcdPrinterException):
     message = """Le produit (code-barres: {brcd}) n'existe pas dans Odoo"""
+    status = 400
     def __init__(self, brcd: str) -> None:
         super().__init__(self.message.format(brcd=brcd))
+        
+class UnknownPrinter(BrcdPrinterException):
+    message = """L'imprimante sélectionnée n'existe pas."""
+    status = 500
+    def __init__(self) -> None:
+        super().__init__(self.message)
+        
+class HintingDesabled(BrcdPrinterException):
+    message = """Hinting désactivé."""
+    status = 400
+    def __init__(self) -> None:
+        super().__init__(self.message)
