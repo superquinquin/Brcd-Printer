f65b7600ca1a82f164c7566b4b8d6276a4886a4a
Fabien VINCENT <git@beufa.fr>
2024-06-17 12:20:29 +0200

    fix(printer/routes.py): add route for img

--- printer/routes.py
+++ printer/routes.py
@@ -53,7 +53,7 @@ async def index(request: Request):
 @printer.get("/header_img")
 async def header_img(request: Request):
 
-    return redirect(f"static/img/barcode_{app.env}.png")
+    return redirect(f"static/img/barcode_{request.app.env}.png")
 
 @printer.post("/job")
 @validator
